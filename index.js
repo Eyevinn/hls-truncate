f60fcad9b8a82aebdb72bd5546dc42655d5a7549
lauta <johan.lautakoski@eyevinn.se>
2023-02-22 21:10:48 +0100

    added begining of support for demuxed audio and change version of m3u8 in package.json

--- index.js
+++ index.js
@@ -8,16 +8,19 @@ class HLSTruncateVod {
     this.playlists = {};
     this.duration = duration;
     this.bandwiths = [];
+    this.audioSegments = {};
   }
 
-  load(_injectMasterManifest, _injectMediaManifest) {
+  load(_injectMasterManifest, _injectMediaManifest, _injectAudioManifest) {
     return new Promise((resolve, reject) => {
       const parser = m3u8.createStream();
 
       parser.on('m3u', m3u => {
         this.m3u = m3u;
         let mediaManifestPromises = [];
+        let audioManifestPromises = [];
         let baseUrl;
+        let audioGroups = {};
         const m = this.masterManifestUri.match(/^(.*)\/.*?$/);
         if (m) {
           baseUrl = m[1] + '/';
@@ -27,26 +30,82 @@ class HLSTruncateVod {
           const streamItem = m3u.items.StreamItem[i];
           this.bandwiths.push(streamItem.get('bandwidth'));
           const mediaManifestUrl = url.resolve(baseUrl, streamItem.get('uri'));
-          mediaManifestPromises.push(this._loadMediaManifest(mediaManifestUrl, streamItem.get('bandwidth'), _injectMediaManifest));
+          if (!m3u.items.MediaItem.find((mediaItem) => mediaItem.get("type") === "AUDIO" && mediaItem.get("uri") == streamItem.get("uri"))) {
+            mediaManifestPromises.push(this._loadMediaManifest(mediaManifestUrl, streamItem.get('bandwidth'), _injectMediaManifest));
+          }
+
+          if (streamItem.attributes.attributes["audio"]) {
+            let audioGroupId = streamItem.attributes.attributes["audio"];
+            if (!this.audioSegments[audioGroupId]) {
+              this.audioSegments[audioGroupId] = {};
+            }
+
+            let audioGroupItems = m3u.items.MediaItem.filter((item) => {
+              return item.attributes.attributes.type === "AUDIO" && item.attributes.attributes["group-id"] === audioGroupId;
+            });
+            // # Find all langs amongst the mediaItems that have this group id.
+            // # It extracts each mediaItems language attribute value.
+            // # ALSO initialize in this.audioSegments a lang. property whos value is an array [{seg1}, {seg2}, ...].
+            let audioLanguages = audioGroupItems.map((item) => {
+              let itemLang;
+              if (!item.attributes.attributes["language"]) {
+                itemLang = item.attributes.attributes["name"];
+              } else {
+                itemLang = item.attributes.attributes["language"];
+              }
+              // Initialize lang. in new group.
+              if (!this.audioSegments[audioGroupId][itemLang]) {
+                this.audioSegments[audioGroupId][itemLang] = [];
+              }
+              return (item = itemLang);
+            });
+
+            // # For each lang, find the lang playlist uri and do _loadAudioManifest() on it.
+            for (let j = 0; j < audioLanguages.length; j++) {
+              let audioLang = audioLanguages[j];
+              let audioUri = audioGroupItems[j].attributes.attributes.uri
+              if (!audioUri) {
+                //# if mediaItems dont have uris
+                let audioVariant = m3u.items.StreamItem.find((item) => {
+                  return !item.attributes.attributes.resolution && item.attributes.attributes["audio"] === audioGroupId;
+                });
+                if (audioVariant) {
+                  audioUri = audioVariant.properties.uri;
+                }
+              }
+              if (audioUri) {
+                let audioManifestUrl = url.resolve(baseUrl, audioUri);
+                if (!audioGroups[audioGroupId]) {
+                  audioGroups[audioGroupId] = {};
+                }
+                // # Prevents 'loading' an audio track with same GroupID and LANG.
+                // # otherwise it just would've loaded OVER the latest occurrent of the LANG in GroupID.
+                if (!audioGroups[audioGroupId][audioLang]) {
+                  audioGroups[audioGroupId][audioLang] = true;
+                  audioManifestPromises.push(this._loadAudioManifest(audioManifestUrl, audioGroupId, audioLang, _injectAudioManifest));
+                }
+              }
+            }
+          }
         }
-          
-        Promise.all(mediaManifestPromises)
-        .then(resolve)
-        .catch(reject);
+
+        Promise.all(mediaManifestPromises.concat(audioManifestPromises))
+          .then(resolve)
+          .catch(reject);
       });
       parser.on('error', (err) => {
         reject("Failed to parse M3U8: " + err);
       });
       if (!_injectMasterManifest) {
         fetch(this.masterManifestUri)
-        .then(res => {
-          res.body.pipe(parser);
-        })
-        .catch(reject);
+          .then(res => {
+            res.body.pipe(parser);
+          })
+          .catch(reject);
       } else {
         _injectMasterManifest().pipe(parser);
       }
-    });  
+    });
   }
 
   getBandwidths() {
@@ -55,6 +114,10 @@ class HLSTruncateVod {
 
   getMediaManifest(bw) {
     return this.playlists[bw].toString();
+  }
+
+  getAudioManifest(audioGroupId, lang) {
+    return this.audioSegments[audioGroupId][lang].toString();
   }
 
   _loadMediaManifest(mediaManifestUri, bandwidth, _injectMediaManifest) {
@@ -68,19 +131,19 @@ class HLSTruncateVod {
         let accDuration = 0;
         let prevAccDuration = 0;
         let pos = 0;
-        
+
         m3u.items.PlaylistItem.map((item => {
           if (accDuration <= this.duration) {
             prevAccDuration = accDuration;
             accDuration += item.get('duration');
             pos++;
-          } 
+          }
         }));
 
         // Logic to find the nearest segment in time:
         // At this stage accDuration is greater than the target duration.
         // If not closer to the target than prevAccDuration, step back a segment.
-        if((accDuration - this.duration) >= ( this.duration - prevAccDuration)) {
+        if ((accDuration - this.duration) >= (this.duration - prevAccDuration)) {
           pos--;
         }
         this.playlists[bandwidth].items.PlaylistItem = m3u.items.PlaylistItem.slice(0, pos);
@@ -91,12 +154,55 @@ class HLSTruncateVod {
       });
       if (!_injectMediaManifest) {
         fetch(mediaManifestUri)
-        .then(res => {
-          res.body.pipe(parser);
-        })
-        .catch(reject);
+          .then(res => {
+            res.body.pipe(parser);
+          })
+          .catch(reject);
       } else {
         _injectMediaManifest(bandwidth).pipe(parser);
+      }
+    });
+  }
+  _loadAudioManifest(audioManifestUri, audioGroupId, audioLang, _injectAudioManifest) {
+    return new Promise((resolve, reject) => {
+      const parser = m3u8.createStream();
+
+      parser.on('m3u', m3u => {
+        if (!this.playlists[audioGroupId][audioLang].length) {
+          this.playlists[audioGroupId][audioLang] = m3u;
+        }
+        let accDuration = 0;
+        let prevAccDuration = 0;
+        let pos = 0;
+
+        m3u.items.PlaylistItem.map((item => {
+          if (accDuration <= this.duration) {
+            prevAccDuration = accDuration;
+            accDuration += item.get('duration');
+            pos++;
+          }
+        }));
+
+        // Logic to find the nearest segment in time:
+        // At this stage accDuration is greater than the target duration.
+        // If not closer to the target than prevAccDuration, step back a segment.
+        if ((accDuration - this.duration) >= (this.duration - prevAccDuration)) {
+          pos--;
+        }
+        this.playlists[audioGroupId][audioLang].items.PlaylistItem = m3u.items.PlaylistItem.slice(0, pos);
+        resolve();
+      });
+      parser.on('error', (err) => {
+        reject("Failed to parse M3U8: " + err);
+      });
+      if (!_injectAudioManifest) {
+        fetch(audioManifestUri)
+          .then(res => {
+            res.body.pipe(parser);
+          })
+          .catch(reject);
+      } else {
+        _injectAudioManifest(audioGroupId).pipe(parser);
       }
     });
   }
